e69b842b44821853088d35198ed13c85836c5f74
Owen Shepherd <owen.shepherd@obsidian.systems>
2020-03-29 15:52:33 +0100

    better port kill handling

--- live.sh
+++ live.sh
@@ -1,7 +1,9 @@
 waitForPort() {
   backoff=0.05
-  echo "waiting for socket to be closed"
+  first=""
   while ss -tulwnH -A tcp | grep 0.0.0.0:8000 >/dev/null; do
+    [[ -z "$first" ]] && echo "waiting for socket to be closed"
+    first="false"
     sleep $backoff
     backoff=$(bc <<< "$backoff * 1.5")
   done
@@ -11,7 +13,13 @@ run() {
   runhaskell Main.hs -d &
   cd ..
 }
-waitForPort
+if ss -tulwnH -A tcp | grep 0.0.0.0:8000 >/dev/null; then
+  read -p "kill process using port 8000 (y/n)? " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    fuser -k 8000/tcp
+  fi
+fi
 run
 sep="-------------------"
 while inotifywait -r -e modify -e create -e delete src 2> /dev/null; do
