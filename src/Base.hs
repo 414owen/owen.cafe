9e19637c7199394cf0be08efc52ddfdd6aeebdfc
Owen Shepherd <owen.shepherd@obsidian.systems>
2020-04-11 01:24:22 +0100

    apple icon

--- src/Base.hs
+++ src/Base.hs
@@ -19,6 +19,7 @@ baseTemplate extraHead title cafeNav rest = pure $ renderHtml $
       meta ! name "viewport" ! content "width=device-width, initial-scale=1"
       meta ! name "theme-color" ! content "#000"
       link ! rel "stylesheet" ! href "/css/default.css"
+      link ! rel "apple-touch-icon" ! href "/favicon.png"
       extraHead
     body $ do
       input ! A.id "hb" ! class_ "hidden" ! type_ "checkbox"
