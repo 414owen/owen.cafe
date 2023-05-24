bbe11e28ef5816c1e8d44aacc8f335755341a7f7
Owen Shepherd <owen@owen.cafe>
2023-05-24 14:47:15 +0200

    feat: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.hugo ];
+        };
+      }
+    );
+}
